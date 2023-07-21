d967d01e89cb362dddf368e19c206e41af5b92c8
yoonchulroh <yoonchulroh@gmail.com>
2023-07-21 17:28:34 +0900

    drag and drop in seaway creation

--- Assets/Scripts/Port/PortBehaviour.cs
+++ Assets/Scripts/Port/PortBehaviour.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PortBehaviour : MonoBehaviour
 {
+    [SerializeField] private GameObject _seawayPrefab;
+    private GameObject _seawayCandidate;
+
     private GameObject _convoySpawner;
     private GameObject _seawaySpawner;
     public Vector3 coordinate;
@@ -43,15 +46,53 @@ public class PortBehaviour : MonoBehaviour
         else if (GameManager.Instance.createManager.createMode == CreateMode.SeawaysOrigin)
         {
             GameManager.Instance.createManager.seawayOrigin = _id;
+            GameManager.Instance.createManager.seawayDestinationCandidate = _id;
             GameManager.Instance.createManager.SwitchCreateMode(CreateMode.SeawaysDestination);
+
+            _seawayCandidate = Instantiate<GameObject>(_seawayPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            _seawayCandidate.GetComponent<LineRenderer>().SetPosition(0, coordinate);
+            _seawayCandidate.GetComponent<LineRenderer>().SetPosition(1, coordinate);
         }
-        else if (GameManager.Instance.createManager.createMode == CreateMode.SeawaysDestination)
+    }
+
+    void OnMouseUp()
+    {
+        if (GameManager.Instance.createManager.createMode == CreateMode.SeawaysDestination)
+        {
+            if (GameManager.Instance.createManager.seawayDestinationCandidate != GameManager.Instance.createManager.seawayOrigin)
+            {
+                _seawaySpawner.GetComponent<SeawaySpawner>().SpawnSeaway(_id, GameManager.Instance.createManager.seawayDestinationCandidate);
+            }
+            Destroy(_seawayCandidate);
+            GameManager.Instance.createManager.SwitchCreateMode(CreateMode.SeawaysOrigin);
+        }
+    }
+
+    void OnMouseDrag()
+    {
+        if (GameManager.Instance.createManager.createMode == CreateMode.SeawaysDestination)
+        {
+            var mousePosition = Camera.main.ScreenToWorldPoint( new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0) );
+            _seawayCandidate.GetComponent<LineRenderer>().SetPosition(1, mousePosition);
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        if (GameManager.Instance.createManager.createMode == CreateMode.SeawaysDestination)
         {
             if (GameManager.Instance.createManager.seawayOrigin != _id)
             {
-                _seawaySpawner.GetComponent<SeawaySpawner>().SpawnSeaway(GameManager.Instance.createManager.seawayOrigin, _id);
-                GameManager.Instance.createManager.SwitchCreateMode(CreateMode.SeawaysOrigin);
+                GameManager.Instance.createManager.seawayDestinationCandidate = _id;
             }
+        }
+    }
+
+    void OnMouseExit()
+    {
+        if (GameManager.Instance.createManager.createMode == CreateMode.SeawaysDestination)
+        {
+            GameManager.Instance.createManager.seawayDestinationCandidate = GameManager.Instance.createManager.seawayOrigin;
         }
     }
 }
